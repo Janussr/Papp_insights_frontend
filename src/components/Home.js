2c349d94f5b70ebf002ce36e230751750f66c037
RasmusDalgaard <cph-rd92@cphbusiness.dk>
2022-05-02 11:18:42 +0200

    Small changes

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,25 +1,24 @@
-import { useState, useEffect } from "react"
-import axios from "axios"
+import { useState, useEffect } from "react";
+import axios from "axios";
+import apiUtils from "../utils/apiUtils";
 
 const Home = () => {
-    const [report, setReport] = useState({});
+  const [report, setReport] = useState({});
+  const URL = apiUtils.getUrl();
+  useEffect(() => {
+    const getReport = async () => {
+      const response = await axios.get(URL);
+      setReport(response.data.hello);
+      console.log(response.data.hello);
+    };
+    getReport();
+  }, [setReport, URL]);
 
-    useEffect(() => {
-        const getReport = async () => {
-          const response = await axios.get(URL)
-          setReport(response.data.hello)
-          console.log(response.data.hello);
-        }
-        getReport()
-      }, [setReport, URL]);
-    
+  return (
+    <div>
+      <h1>Welcome</h1>
+    </div>
+  );
+};
 
-
-    return (
-        <div>
-            <h1>Welcome</h1>
-        </div>
-    )
-}
-
-export default Home
+export default Home;
--- src/utils/apiUtils.js
+++ src/utils/apiUtils.js
@@ -1,26 +1,25 @@
 import axios from "axios";
 
-const URL = 'http://localhost:8000/report';
+const URL = "http://localhost:8000/";
 
 const apiUtils = () => {
+  const getUrl = () => {
+    return URL;
+  };
 
-    const getUrl = () => {
-        return URL;
-    }
+  const getAuthAxios = () => {
+    const authAxios = axios.create({
+      headers: {
+        "x-access-token": localStorage.getItem("jwtToken"),
+      },
+    });
+    return authAxios;
+  };
 
-    const getAuthAxios = () => {
-        const authAxios = axios.create({
-            headers: {
-                'x-access-token': localStorage.getItem('jwtToken')
-            }
-        })
-        return authAxios
-    }
-
-    return {
-        getUrl,
-        getAuthAxios
-    }
-}
+  return {
+    getUrl,
+    getAuthAxios,
+  };
+};
 
 export default apiUtils();
