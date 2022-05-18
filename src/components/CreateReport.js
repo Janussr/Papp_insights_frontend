339f2b03cfb7ed7eaff2e6a2faf09980b6301d2c
FrederikBA <frederikbandersen98@gmail.com>
2022-05-18 10:35:31 +0200

    clean createreport

--- src/components/CreateReport.js
+++ src/components/CreateReport.js
@@ -1,25 +1,17 @@
-import { NavLink, useNavigate } from "react-router-dom";
 import { useState, useEffect } from "react"
 import axios from "axios"
 import apiUtils from "../utils/apiUtils"
-import 'react-date-range/dist/styles.css';
-import 'react-date-range/dist/theme/default.css';
-import 'react-date-range/dist/styles.css'; // main css file
-import 'react-date-range/dist/theme/default.css'; // theme css file
-import React, { Component } from 'react'
 import Select from 'react-select'
 import "react-datepicker/dist/react-datepicker.css";
 import DatePicker from "react-datepicker";
-import uuid from 'react-uuid'
 
-//npm install react-date-range might have to use this if datepicker doesnt work.
+const URL = apiUtils.getUrl()
 
 const CreateReport = () => {
   const [report, setReport] = useState({});
   const [statusMessage, setStatusMessage] = useState("");
   const [parkingAreas, setParkingAreas] = useState([]);
   const [selectedParkingAreas, setSelectedParkingAreas] = useState([]);
-  const URL = apiUtils.getUrl()
 
   const [startDate, setStartDate] = useState(new Date());
   console.log(startDate.getUTCFullYear(), startDate.getUTCMonth() + 1, startDate.getUTCDate())
@@ -27,10 +19,10 @@ const CreateReport = () => {
 
   const handleChange = (e) => {
     const selected = e.map(obj => {
-        return obj.value
+      return obj.value
     })
     setSelectedParkingAreas(selected)
-}
+  }
 
   //Goes through every List in order to show location on the website
   const options = []
@@ -39,14 +31,14 @@ const CreateReport = () => {
     options.push(currentobj)
   }
 
-//Get parking areas
-useEffect(() => {
-  const getParkingArea = async () => {
-    const response = await axios.get(URL + 'parkingareas');
-    setParkingAreas(response.data.parkingareas);
-  };
-  getParkingArea();
-}, [setParkingAreas]);
+  //Get parking areas
+  useEffect(() => {
+    const getParkingArea = async () => {
+      const response = await axios.get(URL + 'parkingareas');
+      setParkingAreas(response.data.parkingareas);
+    };
+    getParkingArea();
+  }, [setParkingAreas]);
 
   const addParkingArea = () => {
     selectedParkingAreas.push({})
@@ -79,15 +71,15 @@ useEffect(() => {
       <h2>Pick location</h2>
 
       <div className="centerContent container">
-      <Select
+        <Select
           onChange={handleChange}
           isMulti
           name="parkingAreas"
           options={options}
           className="basic-multi-select"
           classNamePrefix="select"
-      />
-  </div>
+        />
+      </div>
 
       <form className="date">
         <DatePicker
